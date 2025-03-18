7890628fb308fa6fcf54f1cde0dfaa7dcb445f23
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-03-18 17:40:41 +0000

    Add logging formatter

--- src/tiny_little_service/log.py
+++ src/tiny_little_service/log.py
@@ -1,12 +1,16 @@
 import logging
 
 
+class ServiceFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        record.service_name = "tiny_little_service"
+        return super().format(record)
+
+
 def do_default_logging_setup() -> None:
-    # logger = logging.getLogger("__main__")
     logger = logging.getLogger("tiny_little_service")
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("FOOOO {asctime}", style="{")
-    handler.setFormatter(formatter)
+    handler.setFormatter(ServiceFormatter())
     logger.addHandler(handler)
